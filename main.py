604b2b29cfdc9fa2aac94563276906e7405f3199
Acccjifbhi <siva967763@gmail.com>
2024-07-29 17:46:31 +0530

    init

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel,EmailStr
+app=FastAPI()
+db={}
+
+class create(BaseModel):
+    email:EmailStr
+    password:str
+
+@app.post('/signup')
+def signup(request:create):
+    print(request.email)
+    if request.email not in db:
+        db[request.email]=request.password
+        return {'detail':'signuped successfully'}
+    return {'detail':'email already exists'}
+
+@app.get('/login/{email}/{password}')
+def login(email,password):
+    if email in db:
+        if db[email]==password:
+            return {'detail':f'welcome {email}'}
+    return {'detail':'Incorrect Password or mail'}
